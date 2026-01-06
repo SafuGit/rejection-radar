881ba73a0446ca7ca41c4c75b237654df0bccf16
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2026-01-06 22:20:46 +0600

    feat: add auth guard to Index route for protected access

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,13 @@ import { Routes } from '@angular/router';
 import { Register } from './components/register/register';
 import { VerifyOtp } from './components/verify-otp/verify-otp';
 import { Index } from './components/index';
+import { authGuard } from './guards/auth-guard-guard';
 
 export const routes: Routes = [
   {
     path: '',
-    component: Index
+    component: Index,
+    canActivate: [authGuard]
   },
   {
     path: 'register',
