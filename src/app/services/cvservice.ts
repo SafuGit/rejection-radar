3d4d6fb44fafbcd3ebc07cdd46cca0762cd36d53
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2026-01-07 15:05:57 +0600

    feat: enhance CV upload functionality with structured response handling

--- src/app/services/cvservice.ts
+++ src/app/services/cvservice.ts
@@ -1,8 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CVService {
-  
+  constructor (private http: HttpClient) {}
+  cvData = signal<{message: string, text: string, pages: number, cvJson: any}>({message: '', text: '', pages: 0, cvJson: null});
+
+  uploadCV(file: File) {
+    const formdata = new FormData();
+    formdata.append('cv', file);
+
+    return this.http.post<{message: string, text: string, pages: number, cvJson: any}>('/api/parse-cv', formdata)
+      .subscribe({
+        next: (response) => {
+          this.cvData.set(response);
+        },
+        error: (error) => {
+          console.error('Error uploading CV:', error);
+        }
+      });
+  }
 }
